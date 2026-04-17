4d01fbbee78cd59c6fb70dff21b227a92e3208e0
huutai <huutt201@example.com>
2026-04-17 14:47:47 +0700

    allow CORS_ORIGIN

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,8 +26,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const allowedOrigins = [
+  "*",
   "http://localhost:5173",
-  "https://warning-shoppe-price-five.vercel.app/",
+  "https://warning-shoppe-price-five.vercel.app",
   ...(process.env.CORS_ORIGIN ? [process.env.CORS_ORIGIN] : []),
   ...(process.env.CORS_ORIGINS ?
     process.env.CORS_ORIGINS.split(",")
